78372dc066f92551bb8a9572eed2aabbdc4d8f6c
plekkocoin <Plekkocoin@gmail.com>
2026-08-03 01:23:45 +0330

    Add Plekko tap points

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fish = document.createElement("img");
+
 fish.id = "fish";
 fish.src = "plekko.png";
 fish.style.width = "250px";
@@ -10,14 +11,38 @@ fish.draggable = false;
 
 document.getElementById("fish3d").appendChild(fish);
 
+
+// امتیاز
+let points = 0;
+const pointsText = document.getElementById("points");
+
+
+// دکمه TAP
+document.querySelector(".tap").onclick = () => {
+  points += 1;
+  pointsText.innerText = points;
+};
+
+
+// منوها
 const buttons = document.querySelectorAll(".menu button");
 
-buttons[0].onclick = () => alert("Tasks");
-buttons[1].onclick = () => alert("Invite");
-buttons[2].onclick = () => alert("Rank");
-buttons[3].onclick = () => alert("Gift");
-buttons[4].onclick = () => alert("Ocean");
+buttons[0].onclick = () => {
+  alert("🎯 Tasks: Coming soon");
+};
 
-document.querySelector(".tap").onclick = () => {
-  alert("Tap feature coming soon!");
+buttons[1].onclick = () => {
+  alert("👥 Invite: Coming soon");
+};
+
+buttons[2].onclick = () => {
+  alert("🏆 Rank: Coming soon");
+};
+
+buttons[3].onclick = () => {
+  alert("🎁 Gift: Coming soon");
+};
+
+buttons[4].onclick = () => {
+  alert("🌊 Ocean: Coming soon");
 };
