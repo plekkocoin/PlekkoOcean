07af1fed369cb42308913e966090a670d80d7062
plekkocoin <Plekkocoin@gmail.com>
2026-08-03 01:00:31 +0330

    Add button actions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fish = document.createElement("img");
-
+fish.id = "fish";
 fish.src = "plekko.png";
 fish.style.width = "250px";
 fish.style.maxWidth = "80%";
@@ -9,3 +9,15 @@ fish.style.userSelect = "none";
 fish.draggable = false;
 
 document.getElementById("fish3d").appendChild(fish);
+
+const buttons = document.querySelectorAll(".menu button");
+
+buttons[0].onclick = () => alert("Tasks");
+buttons[1].onclick = () => alert("Invite");
+buttons[2].onclick = () => alert("Rank");
+buttons[3].onclick = () => alert("Gift");
+buttons[4].onclick = () => alert("Ocean");
+
+document.querySelector(".tap").onclick = () => {
+  alert("Tap feature coming soon!");
+};
